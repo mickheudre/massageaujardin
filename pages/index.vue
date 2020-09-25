7d809026857495fc43d3f780e415ee5e02d66e3c
Mick <mickheudre@gmail.com>
2020-09-25 23:45:19 +0200

    ajout d'une mini description

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,40 @@
 <template>
   <!-- <div class="flex flex-col justify-center content-center items-center"> -->
-    <div>
+  <div>
     <div class="flex mt-16">
-  <div class="w-1/2 my-auto">
-      <img class="mt-8 mb-4 mx-auto w-64" alt="Massage pour toutes" src="../assets/img/titre.png">
-    <img class="w-32 mx-auto " src="../assets/img/vague.png">
-    <h6 class="my-8 mx-auto  w-48 text-xl text-duckgreen text-center font-serif">
-      Massage détente, écoute et plantes
-    </h6>
+      <div class="w-1/2 my-auto">
+        <img class="mt-8 mb-4 mx-auto w-64" alt="Massage pour toutes" src="../assets/img/titre.png">
+        <img class="w-32 mx-auto " src="../assets/img/vague.png">
+        <h6 class="my-8 mx-auto  w-48 text-xl text-duckgreen text-center font-serif">
+          Massage détente, écoute et plantes
+        </h6>
+      </div>
+      <div class="w-1/2">
+        <img class="w-3/5 mx-auto" alt="Une illustration de deux mains" src="../assets/img/logo.png">
+      </div>
+    </div>
+    <h5 class="mt-16 text-duckgreen text-lg text-center">
+      Le site est en construction
+    </h5>
   </div>
-  <div class="w-1/2">
-    <img class="w-3/5 mx-auto" alt="Une illustration de deux mains" src="../assets/img/logo.png">
-  </div>
-</div>
-  <h5 class="mt-16 text-duckgreen text-lg text-center">Le site est en construction</h5>
-</div>
 </template>
 
 <script>
-export default {}
+export default {
+  head () {
+    return {
+      title: 'Massage pour toutes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Massage détente, écoute et plantes. Les rendez-vous se font à Fontaine 38600. Tarification solidaire.'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style>
